2e31c27c61db8451a050428376560979a70ce3ec
HanselRm <20220897@itla.edu.do>
2025-04-08 14:49:38 -0400

    interfaz de repositorios

---
+++ DGAPrueba.Core.Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DGAPrueba.Core.Application.Interfaces.Repositories;
+
+public interface IBaseRepository<T>
+{
+    //Repositorio generico    
+    Task<List<T>> GetAllAsync();
+    Task<List<T>> GetAllWithIncludeAsync(List<string> includes);
+    Task<T> GetByIdAsync(int id);
+    Task<T> SaveAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+}
