5590faa988ec430042152ae3a93d3602a53e5da2
HanselRm <20220897@itla.edu.do>
2025-04-08 09:44:53 -0400

    DbContext

---
+++ DGAPrueba.Infrastructure.Persistence/Context/DGAContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DGAPrueba.Infrastructure.Persistence.Context;
+
+//contexto de la base de datos 
+//se encarga de la comunicacion entre la base de datos y el sistema
+public class DGAContext : DbContext
+{
+    public DGAContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+}
