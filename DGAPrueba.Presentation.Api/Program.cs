aa524fb458b6647ee7ed3aa26b7dbc0a970cb628
HanselRm <20220897@itla.edu.do>
2025-04-08 11:24:32 -0400

    extension method

--- DGAPrueba.Presentation.Api/Program.cs
+++ DGAPrueba.Presentation.Api/Program.cs
@@ -1,9 +1,13 @@
+using DGAPrueba.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Persistence Layer
+builder.Services.AddPersistenceLayer(builder.Configuration);
 
 var app = builder.Build();
 
