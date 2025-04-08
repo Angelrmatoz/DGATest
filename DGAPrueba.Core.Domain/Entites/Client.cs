1bea2df68bf06b01201a6a042540b85db1dadff7
HanselRm <20220897@itla.edu.do>
2025-04-08 09:52:58 -0400

    navigation property

--- DGAPrueba.Core.Domain/Entites/Client.cs
+++ DGAPrueba.Core.Domain/Entites/Client.cs
@@ -8,4 +8,7 @@ public class Client : BaseEntity
     public string Name { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
+    
+    //navigation property
+    public List<Sales> Sales { get; set; }
 }
