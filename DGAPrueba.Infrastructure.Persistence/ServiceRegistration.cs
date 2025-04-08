8b62938f1586fee87ceb5da1ef285d3f5abf40d2
HanselRm <20220897@itla.edu.do>
2025-04-08 11:22:43 -0400

    extension method

---
+++ DGAPrueba.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using DGAPrueba.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DGAPrueba.Infrastructure.Persistence;
+
+public static class ServiceRegistration
+{
+    // Extension de metodo para registrar el contexto de la base de datos 
+    public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region Context
+            services.AddDbContext<DGAContext>(op => op.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                m => m.MigrationsAssembly(typeof(DGAContext).Assembly.FullName)));
+        #endregion
+    }
+}
