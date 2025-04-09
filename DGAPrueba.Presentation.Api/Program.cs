76a56bd2ae2302a91fd938f87fbf80e137ed722b
HanselRm <20220897@itla.edu.do>
2025-04-09 08:41:43 -0400

    call methods for services registration

--- DGAPrueba.Presentation.Api/Program.cs
+++ DGAPrueba.Presentation.Api/Program.cs
@@ -1,4 +1,7 @@
+using DGAPrueba.Core.Application;
 using DGAPrueba.Infrastructure.Persistence;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,36 +12,39 @@ builder.Services.AddSwaggerGen();
 // Persistence Layer
 builder.Services.AddPersistenceLayer(builder.Configuration);
 
+//Application Layer
+builder.Services.AddApplicationLayer();
+
+builder.Services.AddControllers();
+
+//Swagger configuration
+builder.Services.AddSwaggerGen(op =>
+{
+    op.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "DGAPrueba API",
+        Description = "Test para la prueba de la DGA",
+        Contact = new OpenApiContact
+        {
+            Name = "Hansel Rodriguez"
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+    //Swagger default route
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
 
 app.Run();
 
