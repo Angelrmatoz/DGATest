121e17a9371befe994e128a785bc9ba8f8415b5b
HanselRm <20220897@itla.edu.do>
2025-04-10 09:53:05 -0400

    add properties

--- DGAPrueba.Core.Domain/Entites/SaleProduct.cs
+++ DGAPrueba.Core.Domain/Entites/SaleProduct.cs
@@ -4,11 +4,14 @@ namespace DGAPrueba.Core.Domain.Entites;
 
 public class SaleProduct : BaseEntity
 {
-    public int SalesId { get; set; }
-    public int ProductId { get; set; }
+    public decimal Price { get; set; }
     public int Quantity { get; set; }
     
-    //Navegacion
+    //relacion de ventas
+    public int SalesId { get; set; }
     public Sales Sales { get; set; }
+    
+    //relacion de productos
+    public int ProductId { get; set; }
     public Product Product { get; set; }
 }
