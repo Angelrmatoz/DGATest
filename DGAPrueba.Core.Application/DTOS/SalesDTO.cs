1dae4fc1c7db235ea4017697a53797c371498f8c
HanselRm <20220897@itla.edu.do>
2025-04-10 13:56:38 -0400

    add properties to DTOS

--- DGAPrueba.Core.Application/DTOS/SalesDTO.cs
+++ DGAPrueba.Core.Application/DTOS/SalesDTO.cs
@@ -6,4 +6,5 @@ public class SalesDTO
     public DateOnly Date { get; set; }
     public double Total { get; set; }
     public int ClientId { get; set; }
+    public List<SaveSaleProductDTO> Products { get; set; } = new();
 }
--- DGAPrueba.Core.Application/DTOS/SaveSaleProductDTO.cs
+++ DGAPrueba.Core.Application/DTOS/SaveSaleProductDTO.cs
@@ -6,5 +6,6 @@ public class SaveSaleProductDTO
     public int SalesId { get; set; }
     public int ProductId { get; set; }
     public int Quantity { get; set; }
+    public decimal Price { get; set; }
 
 }
