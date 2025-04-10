e8d9154ec4528d109f526f99849829d38d840aea
HanselRm <20220897@itla.edu.do>
2025-04-10 13:42:25 -0400

    add custom save method

--- DGAPrueba.Core.Application/Services/SaleServices.cs
+++ DGAPrueba.Core.Application/Services/SaleServices.cs
@@ -9,10 +9,45 @@ namespace DGAPrueba.Core.Application.Services;
 public class SaleServices : BaseServices<SalesDTO, Sales>, ISalesServices
 {
     private readonly ISaleRepository _salesRepository;
-    private readonly IMapper _mapper;
-    
-    public SaleServices(ISaleRepository salesRepository, IMapper mapper) : base(salesRepository, mapper)
+    private readonly ISaleProductServices _saleProductServices;
+
+private readonly IMapper _mapper;
+
+    public SaleServices(ISaleRepository salesRepository, IMapper mapper, ISaleProductServices saleProductServices) : base(salesRepository, mapper)
     {
         _salesRepository = salesRepository;
+        _mapper = mapper;
+        _saleProductServices = saleProductServices;
+    }
+
+    public  override async Task<Sales> SaveAsync(SalesDTO saveSalesDTO)
+    {
+        // Mapear el DTO a la entidad
+        var sales = _mapper.Map<Sales>(saveSalesDTO);
+        
+        // Guardar la entidad en el repositorio
+        
+        var entitySave = await _salesRepository.SaveAsync(sales);
+        
+        // verificar si la entidad se guardó correctamente
+        if (entitySave == null)
+        {
+            throw new Exception($"No se pudo guardar el registro");
+        }
+        
+        // Guardar los productos de la venta
+        foreach (var saleProduct in saveSalesDTO.Products)
+        {
+            var saleProductDTO = new SaveSaleProductDTO
+            {
+                ProductId = saleProduct.ProductId,
+                SalesId = entitySave.Id,
+                Quantity = saleProduct.Quantity
+            };
+            
+            // Guardar el producto de la venta
+            await _saleProductServices.SaveAsync(saleProductDTO);
+        }
+        return entitySave;
     }
 }
