4477f4115ee78b642210f5e67f3d6fddb36fdd69
HanselRm <20220897@itla.edu.do>
2025-04-11 07:55:38 -0400

    DTOS para usuarios

---
+++ DGAPrueba.Core.Application/DTOS/Account/AuthenticationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DGAPrueba.Core.Application.DTOS.Client.Account;
+
+public class AuthenticationRequest
+{
+    // properties
+    public string email { get; set; }
+    public string Password { get; set; }
+}
---
+++ DGAPrueba.Core.Application/DTOS/Account/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace DGAPrueba.Core.Application.DTOS.Client.Account;
+
+public class RegisterRequest
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+    
+}
