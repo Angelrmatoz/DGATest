d2526d25a2147653d954e89f7883ce91927ea90a
HanselRm <20220897@itla.edu.do>
2025-04-10 20:52:47 -0400

    custom entity user

---
+++ sDGAPrueba.Infrastructure.Identity/Model/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace sDGAPrueba.Infrastructure.Identity.Model;
+
+// Modelo custom de identity
+public class ApplicationUser : IdentityUser
+{
+    public string name { get; set; }
+    public string lastName { get; set; }
+}
