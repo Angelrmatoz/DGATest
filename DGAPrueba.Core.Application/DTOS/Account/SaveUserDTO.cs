df68937b6e40b1f834364a99b56f8d80249f2647
HanselRm <20220897@itla.edu.do>
2025-04-11 07:56:05 -0400

    save user dto

---
+++ DGAPrueba.Core.Application/DTOS/Account/SaveUserDTO.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace DGAPrueba.Core.Application.DTOS.Client.Account;
+
+public class SaveUserDTO
+{
+    [SwaggerParameter("Id del usuario")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Debes ingresar tu Nombre")]
+        [SwaggerParameter("Nombre de la persona")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar tu Apellido")]
+        [SwaggerParameter("Apellido de la persona")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar el numero de telefono")]
+        [SwaggerParameter("Numero de telefono")]
+        public string PhoneNumber { get; set; }
+
+        
+        [Required(ErrorMessage = "Debes Ingresar un Usuario")]
+        [SwaggerParameter("Nombre de usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Debes Ingresar tu Correo")]
+        [SwaggerParameter("Correo electronico")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe colocar una contraseña")]
+        [SwaggerParameter("Contraseña")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Las contraseñas no coiciden")]
+        [Required(ErrorMessage = "Debe colocar una contraseña")]
+        public string ConfirmPassword { get; set; }
+
+        [JsonIgnore]
+        public bool IsConfirm { get; set; }
+        [JsonIgnore]
+        public bool? HasError { get; set; }
+        [JsonIgnore]
+        public string? Error { get; set; }
+}
