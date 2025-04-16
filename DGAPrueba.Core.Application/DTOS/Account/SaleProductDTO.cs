cd36bfcf565f1d8c9c6c75a88206fd63feadccea
HanselRm <20220897@itla.edu.do>
2025-04-16 13:27:54 -0400

    add Quantity properties

--- DGAPrueba.Core.Application/DTOS/Account/SaleProductDTO.cs
+++ DGAPrueba.Core.Application/DTOS/Account/SaleProductDTO.cs
@@ -5,6 +5,7 @@ public class SaleProductDTO
 {
     public int ID { get; set; }
     public string Name { get; set; }
+    public int Quantity { get; set; }
     public Product Product { get; set; }
     public Sales Sales { get; set; }
 }
