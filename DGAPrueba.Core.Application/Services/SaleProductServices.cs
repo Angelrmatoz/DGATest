9c4bb1623a4306b5896b4e1ec1ee74a2eae25d6a
HanselRm <20220897@itla.edu.do>
2025-04-16 13:28:21 -0400

    GetAllWithIncludeAsync method

--- DGAPrueba.Core.Application/Services/SaleProductServices.cs
+++ DGAPrueba.Core.Application/Services/SaleProductServices.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using DGAPrueba.Core.Application.DTOS.Account;
 using DGAPrueba.Core.Application.DTOS.Client;
 using DGAPrueba.Core.Application.Interfaces.Repositories;
 using DGAPrueba.Core.Application.Interfaces.Services;
@@ -14,5 +15,20 @@ public class SaleProductServices : BaseServices<SaveSaleProductDTO, SaleProduct>
     public SaleProductServices(ISalesProductRepository salesProductRepository, IMapper mapper) : base(salesProductRepository, mapper)
     {
         _salesProductRepository = salesProductRepository;
+    }
+    
+    //metodo sobreescrito get con include
+    public  async Task<List<SaleProductDTO>> GetAllWithIncludeAsync()
+    {
+        // Obtener todos los productos de la base de datos
+        var propierties = await _salesProductRepository.GetAllWithIncludeAsync(new List<string>{"Sales", "Product"});
+        // Mapear la lista de entidades a una lista de DTOs
+        return propierties.Select(p => new SaleProductDTO
+        {
+            ID = p.Id,
+            Quantity = p.Quantity,
+            Product = p.Product,
+            Sales = p.Sales,
+        }).ToList();
     }
 }
