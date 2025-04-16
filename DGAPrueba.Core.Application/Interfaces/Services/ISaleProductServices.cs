041fc5afc1c743395649c420e04f3cd25651fc9e
HanselRm <20220897@itla.edu.do>
2025-04-16 09:51:31 -0400

    custom method

--- DGAPrueba.Core.Application/Interfaces/Services/ISaleProductServices.cs
+++ DGAPrueba.Core.Application/Interfaces/Services/ISaleProductServices.cs
@@ -1,9 +1,11 @@
-﻿using DGAPrueba.Core.Application.DTOS.Client;
+﻿using DGAPrueba.Core.Application.DTOS.Account;
+using DGAPrueba.Core.Application.DTOS.Client;
 using DGAPrueba.Core.Domain.Entites;
 
 namespace DGAPrueba.Core.Application.Interfaces.Services;
 
 public interface ISaleProductServices : IBaseService<SaveSaleProductDTO, SaleProduct>
 {
-    
+    Task<List<SaleProductDTO>> GetAllWithIncludeAsync();
+
 }
